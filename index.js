9905c1c479842572c9e9b09594cb967f06150ef7
Lewis Benson <105423307+tm-LBenson@users.noreply.github.com>
2024-06-21 15:45:27 +0000

    rename challenge 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
-import challenge1 from './challenges/CalculatingWithFunctions.js'
-
+import challenge1 from './challenges/calculatingWithFunctions.js'
+import challenge2 from './challenges/currying.js'
 // challenge1()
+
+    console.log(challenge2(1)(4)(1)())
+    
